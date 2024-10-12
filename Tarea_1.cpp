eac36abccaaeccb30d1472e42ecdebdd2343c5e8
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-10-12 00:56:51 -0300

    Creación de los autos de carreras

--- Tarea_1.cpp
+++ Tarea_1.cpp
@@ -2,46 +2,41 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<conio.h>
+#include<vector>
+#include<random>
 
 using namespace std;
 
-/*void *procesoSeparado(void *data){
+void *pistaDeCarreras(void *data){
 
-    int n = 0;
-    char *texto = (char *) data;
-    struct timespec tiempo = {1,0};
-    while(n != 10){
-                cout<<texto<<endl;
-                pthread_delay_np(&tiempo);
-                n++;
-
-    }
-
-
-}*/
+        
+}
 
 int main(){
 
 
     cout<<"\n\n-----------------------------------------------\n\n"<<endl;
 
-    /*int metros = 0, C_autos = 0;
+    int metros = 0, C_autos = 0;
 
     cout<<"Ingrese la cantidad de autos:     ";
     cin>>C_autos;
 
-    array<pthread_t> autos(C_autos);
+    //Se crea la hebra de tipo vector de tamaño n (C_autos) que es ingresado por el usuario
+    vector<pthread_t> autos(C_autos);
 
+    //Crea las hebras con el nombre de autos acompañado con un número discriminador de i+1
     for(int i = 0; i<C_autos;i++){
-        
-    }*7
-   /* pthread_t proceso1;
-    pthread_t proceso2;
-    pthread_create(&proceso1, NULL, &procesoSeparado,(void*)"Hola");
-    pthread_create(&proceso2, NULL, &procesoSeparado,(void*)"Adios");
-    pthread_join(proceso1,NULL);
-    pthread_join(proceso2,NULL);
-    */
+        string Cars = "Auto " + to_string(i+1);
+        pthread_create(&autos[i],NULL,&pistaDeCarreras,(void*)Cars.c_str());
+
+    }
+
+    //Aquí unimos todas las hebras
+    for(int i=0; i<C_autos;i++){
+        pthread_join(autos[i],NULL);
+    }
+
 
 
     cout<<"\n\nHola Mundo :D\n\n"<<endl;
