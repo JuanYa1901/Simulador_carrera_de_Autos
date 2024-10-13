a0cb461507030d76071c47b14c4a56d38b763368
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-10-13 16:25:51 -0300

    CAMBIOS AGREGADOS
    Cambio en l estructura de autoInformation, ahora es de tipo String el nombreAuto. Se comienza a trabajar con los arreglos dinamicos tipo vector<> para agregar los autos que terminan la carrera al ranking. Se comienza a trabajar con la libreria mutex para trabajar en los autos que terminen la carrera y controlar la consola. Ahora funciona la funcion de los thread y sale el texto de que los autos avanzan. Se agregó un for para imprimir el ranking de los autos

--- Tarea_1.cpp
+++ Tarea_1.cpp
@@ -1,79 +1,103 @@
-#include<iostream>
-#include<stdlib.h>
-#include<pthread.h>
-#include<conio.h>
-#include<vector>
-#include<unistd.h>
-#include<ctime>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <vector>
+#include <unistd.h>
+#include <ctime>
+#include <mutex>
 
 using namespace std;
 
+// Estructura para guardar la información de los autos
 struct autoInformation {
-
-    const char* nombreAuto;
+    string nombreAuto;
     int pista;
-
 };
 
-void *pistaDeCarreras(void *data){
+// Vector para almacenar el ranking de llegada
+vector<string> ranking;
 
+// Mutex para proteger el acceso al ranking
+mutex rankingMutex;
+// Nuevo mutex para proteger la consola
+mutex consoleMutex;  
+
+// Función que ejecuta cada hebra (cada auto)
+void* pistaDeCarreras(void* data) {
+	
+	srand(time(0) + pthread_self());  
+	
     autoInformation* carData = static_cast<autoInformation*>(data);
-    const char * nombreAuto = carData -> nombreAuto; 
-    
+    const string& nombreAuto = carData->nombreAuto;
 
     int metros_avanzados = 0;
-    int meta = carData -> pista;
+    int meta = carData->pista;
+    
+    
 
-    while(metros_avanzados < meta){
-
-        int avanzar = rand()%10+1;
-
+    while (metros_avanzados <= meta) {
+    	
+        int avanzar = rand() % 10 + 1; 
         metros_avanzados += avanzar;
 
-        cout << nombreAuto << " avanzó" << avanzar << "metros.  (Total: )"<<metros_avanzados<<" metros"<<endl;
-        sleep(1);
-
+        // Bloquear la consola para evitar interferencias
+		consoleMutex.lock(); 
+        
+        // Mostrar el avance del auto
+        cout << nombreAuto << " avanza " << avanzar << " metros. ---------- (Total: " << metros_avanzados << " metros)" << endl;
+		consoleMutex.unlock();  // Desbloquear la consola
+		 
+        sleep(4);
+        
+        cout<<"\n";
     }
 
-    cout<<nombreAuto << " Ha llegado a la meta!" <<endl;
-    return nullptr;
+    // Cuando el auto llega a la meta, lo agregamos al ranking
+    rankingMutex.lock();  // Bloqueamos el acceso al ranking
+    ranking.push_back(nombreAuto);  // Agregamos el nombre del auto al ranking
+    rankingMutex.unlock();  // Desbloqueamos el acceso al ranking
 
+    cout << nombreAuto << " ha llegado a la meta!" << endl;
+    return nullptr;
 }
 
-int main(){
-
-
-    cout<<"\n\n-----------------------------------------------\n\n"<<endl;
-
+int main() {
+    cout << "\n\n-----------------------------------------------\n\n" << endl;
+	srand(time(0));
     int pista, C_autos;
 
-    srand(time(0));
-    
-    cout<<"Ingrese el largo de la pista de carreras:     ";
-    cin>>pista;
+    // Ingresar los datos de la pista y la cantidad de autos
+    cout << "Ingrese el largo de la pista de carreras (metros): ";
+    cin >> pista;
 
-    cout<<"Ingrese la cantidad de autos:     ";
-    cin>>C_autos;
+    cout << "Ingrese la cantidad de autos: ";
+    cin >> C_autos;
 
-    //Se crea la hebra de tipo vector de tamaño n (C_autos) que es ingresado por el usuario
+cout<<"\n";
+cout<<"\n";
+    // Vector de hebras y vector para los datos de cada auto
     vector<pthread_t> autos(C_autos);
+    vector<autoInformation> carData(C_autos);
 
-    //Crea las hebras con el nombre de autos acompañado con un número discriminador de i+1
-    for(int i = 0; i<C_autos;i++){
-        string Cars = "Auto" + to_string(i+1);
-        pthread_create(&autos[i],NULL,&pistaDeCarreras,(void*)Cars.c_str());
-
+    // Crear las hebras con el nombre de cada auto
+    for (int i = 0; i < C_autos; i++) {
+        carData[i].nombreAuto = "Auto " + to_string(i + 1);
+        carData[i].pista = pista;
+        pthread_create(&autos[i], NULL, &pistaDeCarreras, (void*)&carData[i]);
     }
 
-    //Aquí unimos todas las hebras
-    for(int i=0; i<C_autos;i++){
-        pthread_join(autos[i],NULL);
+    // Unir todas las hebras (esperar que todos los autos terminen)
+    for (int i = 0; i < C_autos; i++) {
+        pthread_join(autos[i], NULL);
     }
 
+    // Mostrar el ranking de la carrera
+    cout << "\nRanking final:\n";
+    for (size_t i = 0; i < ranking.size(); ++i) {
+        cout << i + 1 << ". " << ranking[i] << endl;
+    }
 
+    cout << "\n\nFin de la carrera!\n\n" << endl;
 
-
-    getch();
     return 0;
 }
